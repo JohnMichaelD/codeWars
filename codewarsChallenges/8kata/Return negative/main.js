705046f1cf1ee544661474576e443b7a69c02c9d
John Darrin <john.m.darrin@gmail.com>
2022-04-29 18:30:09 -0400

    string repeat

--- codewarsChallenges/8kata/Return negative/main.js
+++ codewarsChallenges/8kata/Return negative/main.js
@@ -13,4 +13,12 @@ Notes
 
  */
 
-    
+    function makeNegative(num) {
+        if(num > 0){
+          return num * -1;
+        } else if (num < 0){
+          return num;
+        } else {
+          return num;
+        }
+      }
