e142d94a0a1526d107496aa6be3c6c345deab9c9
John Darrin <john.m.darrin@gmail.com>
2022-05-04 12:51:53 -0400

    reversed strings using methods, recursion, and for loop

---
+++ codewarsChallenges/8kata/Reversed Strings/main.js
@@ -0,0 +1,88 @@
+/*Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+*/
+
+//What I can do:
+
+function solution(str){
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--){
+      newString += str[i];
+    } return newString;
+  }
+
+//Using string and array methods:
+
+function reverseString(str) {
+    // Step 1. Use the split() method to return a new array
+    var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+ 
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+ 
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+    
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
+}
+ 
+reverseString("hello");
+
+
+//Chaining the three methods together:
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+reverseString("hello");
+
+// Using Recursion
+
+function reverseString(str) {
+    if (str === "") // This is the terminal case that will end the recursion
+      return "";
+    
+    else
+      return reverseString(str.substr(1)) + str.charAt(0);
+  /* 
+  First Part of the recursion method
+  You need to remember that you won’t have just one call, you’ll have several nested calls
+  
+  Each call: str === "?"        	                  reverseString(str.subst(1))     + str.charAt(0)
+  1st call – reverseString("Hello")   will return   reverseString("ello")           + "h"
+  2nd call – reverseString("ello")    will return   reverseString("llo")            + "e"
+  3rd call – reverseString("llo")     will return   reverseString("lo")             + "l"
+  4th call – reverseString("lo")      will return   reverseString("o")              + "l"
+  5th call – reverseString("o")       will return   reverseString("")               + "o"
+  
+  Second part of the recursion method
+  The method hits the if condition and the most highly nested call returns immediately
+  
+  5th call will return reverseString("") + "o" = "o"
+  4th call will return reverseString("o") + "l" = "o" + "l"
+  3rd call will return reverseString("lo") + "l" = "o" + "l" + "l"
+  2nd call will return reverserString("llo") + "e" = "o" + "l" + "l" + "e"
+  1st call will return reverserString("ello") + "h" = "o" + "l" + "l" + "e" + "h" 
+  */
+  }
+  reverseString("hello");
+
+//Without Comments
+
+  function reverseString(str) {
+    if (str === "")
+      return "";
+    else
+      return reverseString(str.substr(1)) + str.charAt(0);
+  }
+  reverseString("hello");
+
+
+  //Solutions from: https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
