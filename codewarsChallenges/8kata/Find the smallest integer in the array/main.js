1912f3c802af8233f1a0a589b96f7998d7d09e91
John Darrin <john.m.darrin@gmail.com>
2022-05-07 13:35:18 -0400

    Math.min ...

--- codewarsChallenges/8kata/Find the smallest integer in the array/main.js
+++ codewarsChallenges/8kata/Find the smallest integer in the array/main.js
@@ -8,3 +8,8 @@ For example:
 You can assume, for the purpose of this kata, that the supplied array will not be empty.
  */
 
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args);
+    }
+  }
